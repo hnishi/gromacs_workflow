0db58703c8f81bb0ddcf50b744da4d85d2db722f
hnishi <hnishigami@protein.osaka-u.ac.jp>
2018-01-26 17:10:36 +0900

    Create do_trjconv.bash

---
+++ umbrella_sampling/02smd/do_trjconv.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+awk '{if($1==i){print $1/2,$3;i=i+2}}' md1_pullx.xvg > summary_z.dat
+
+mkdir -p ./pullconfs
+cd ./pullconfs
+trjconv -s ../md1.tpr -f ../md1.xtc -o conf.gro -sep <<< 'System'
+perl ../distances.pl
+cd ..
+
