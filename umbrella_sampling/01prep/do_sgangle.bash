4c2e6fc81266b5f8973a89c69f63e6e028495bcd
hnishi <hnishigami@protein.osaka-u.ac.jp>
2018-01-26 16:56:29 +0900

    Create do_sgangle.bash

---
+++ umbrella_sampling/01prep/do_sgangle.bash
@@ -0,0 +1,4 @@
+g_sgangle_d -f min2.gro -s min2.tpr -n index_axrst.ndx -z -oa sg_angle_sele1.xvg <<< a_2692_4493 
+g_sgangle_d -f min2.gro -s min2.tpr -n index_axrst.ndx -z -oa sg_angle_sele2.xvg <<< a_3303_3905
+
+
